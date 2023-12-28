4f9d5624c84a491420713c29039c63c7e6187453
Arina Udina <arisha.udina@gmail.com>
2023-12-28 16:09:34 +0300

    file correction
    Signed-off-by: Arina Udina <arisha.udina@gmail.com>

--- Students/Students/students.cpp
+++ Students/Students/students.cpp
@@ -6,40 +6,42 @@ using namespace std;
 struct Student {
 	char FIO[41];
 	int course;
-	double avg_rating;
+	double avgRating;
 };
 
-struct Student_ext {
+struct StudentExt {
 	char FIO[41];
 	int course;
-	double avg_rating;
+	double avgRating;
 
 	struct {
 		char name[36];
 		int grade;
 	} subjects[2];
 };
-int main() {
-	
 
+
+int main() {
 	Student iam = { "Udina A.B.", 10, 89.7 };
-	Student_ext other = {
+	StudentExt other = {
 	 "Marihewa S.M.", 9, 199.7,
 		{
 		  {"biology", 4},
 		   {"chemistry", 5}
 		   }
 	};
+
 	iam.course++;
 	puts(iam.FIO);
 	strcpy_s(other.subjects[0].name, "Philosophy");
 	other.subjects[0].grade = 100;
 	
-	if (other.avg_rating <= iam.avg_rating) {
-	    cout << iam.FIO << iam.avg_rating;
+	if (other.avgRating <= iam.avgRating) {
+	    cout << iam.FIO << iam.avgRating;
 	}
 	else {
-	    cout << other.FIO << other.avg_rating;
+	    cout << other.FIO << other.avgRating;
 	}
-	
+
+	return 0;
 }
