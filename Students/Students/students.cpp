67833e77141027a198cca868cb2df02605a34a60
Arina Udina <arisha.udina@gmail.com>
2023-12-28 19:00:57 +0300

    refactoring

--- Students/Students/students.cpp
+++ Students/Students/students.cpp
@@ -22,30 +22,43 @@ struct StudentExt {
 };
 
 
+void printStudent(const Student& student) {
+    cout << "FIO: " << student.FIO << ", course: " << student.course << ", avgRating: " << student.avgRating << endl;
+}
+
 int main() {
     // Task 1
+    cout << "Task 1" << endl;
     Student iam = { "Udina A.B.", 10, 89.7 }; 
+    cout << "I am:" << endl;
+    printStudent(iam);
     iam.course++;
-    cout << iam.FIO << endl;
+    cout << "New course:" << endl;
+    printStudent(iam);
 
     // Task 2
+    cout << "\nTask 2" << endl;
     Student students[25];
     for (int i = 0; i < 25; ++i) {
         sprintf_s(students[i].FIO, "student %d", i);
         students[i].course = 10;
         students[i].avgRating = rand() % 100;
+        printStudent(students[i]);
     }
 
     Student* topStudent = &students[0]; 
     for (int i = 1; i < 25; ++i) { 
         if (topStudent->avgRating < students[i].avgRating) {
             topStudent = &students[i];
+            cout << "New top:" << endl;
+            printStudent(*topStudent);
         }
     }
     cout << "Top student:" << topStudent->FIO << " " << topStudent->avgRating << endl;
     topStudent = nullptr; // NULL for C
 
     // Task 3
+    cout << "\nTask 3" << endl;
     StudentExt other = { 
         "Marihewa S.M.", 9, 199.7,
         {
@@ -53,8 +66,10 @@ int main() {
             {"chemistry", 25}
         }
     };
+    cout << "src:" << other.subjects[0].name << " " << other.subjects[0].grade << endl;
     strcpy_s(other.subjects[0].name, "philosophy");
     other.subjects[0].grade = 100;
+    cout << "dst:" << other.subjects[0].name << " " << other.subjects[0].grade << endl;
 
     return 0;
 }
